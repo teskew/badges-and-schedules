bb5c316a9985f1aa71c2d8d3184e15c5282e8b09
teskew <teskinder1@gmail.com>
2020-11-18 11:04:40 -0600

    commit tree 671339c1a3807360dc635e2dbc244fa32d871025

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+    
+   "Hello, my name is #{name}."  
+end
+def batch_badge_creator(arr)
+    new_array = []
+    arr.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+    end
+     new_array
+ end
+ def assign_rooms(arr)
+    new_array = []
+    counter = 1
+    arr.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{counter }!")
+    counter +=1
+    end
+     new_array
+ end
+
+
+
+def printer(arr)
+    batch_badge_creator(arr).each do |badge|
+    puts badge
+end
+    assign_rooms(arr).each do |assigment|
+        puts assigment
+
+end
+end
